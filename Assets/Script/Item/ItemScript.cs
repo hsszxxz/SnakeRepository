7a18bb5782889ccff0b347ba256940b5f0c6e75a
hsszxxz <154242074+hsszxxz@users.noreply.github.com>
2024-10-21 13:49:54 +0800

    物品展示

--- Assets/Script/Item/ItemScript.cs
+++ Assets/Script/Item/ItemScript.cs
@@ -6,19 +6,29 @@ public class ItemScript : MonoBehaviour
 {
     private Image image;
     [HideInInspector]
-    public string name;
-    [HideInInspector]
     public int num = 0;
     [HideInInspector]
     public int id;
+    private ItemDataBase data;
+
+    private Button button;
     private void Awake()
     {
+        button = GetComponent<Button>();
         image = GetComponent<Image>();
+        button.onClick.AddListener(ShowDetails);
+    }
+    private void ShowDetails()
+    {
+        UIManager.Instance.GetUIWindow<BagUIWindow>().ShutAndOpen(false);
+        ShowUIWindow showUIWindow = UIManager.Instance.GetUIWindow<ShowUIWindow>();
+        showUIWindow.ShutAndOpen(true);
+        showUIWindow.ShowItem(data);
     }
     public void InitItem(ItemDataBase dataBase)
     {
+        data = dataBase;
         image.sprite = dataBase.img;
-        name = dataBase.name;
         id = dataBase.id;
         num = 1;
     }
--- Assets/Script/UIControl/UIAssemble/ShowUIWindow.cs
+++ Assets/Script/UIControl/UIAssemble/ShowUIWindow.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 public class ShowUIWindow:UIWindow
 {
     public Image itemImage;
+    public Text describe;
+    public Button cancel;
+    private void Start()
+    {
+        cancel.onClick.AddListener(CloseWindow);
+    }
+    private void CloseWindow()
+    {
+        Time.timeScale = 1.0f;
+        ShutAndOpen(false);
+    }
     public void ShowItem(ItemDataBase item)
     {
+        Time.timeScale = 0f;
         itemImage.sprite = item.img;
+        describe.text = item.description;
     }
 }
 
